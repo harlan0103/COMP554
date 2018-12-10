fdc3b628259f1369470d96aa54de3c3236eceb55
harlan0103 <harlan0103@gmail.com>
2018-12-09 21:11:19 -0600

    554

--- plot.py
+++ plot.py
@@ -1,27 +1,30 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Dec  3 00:40:56 2018
+Created on Sun Dec  2 22:05:55 2018
 
-@author: Lenovo
+@author: dinghao
 """
 
 import csv
 import matplotlib.pyplot as plt
+import math
 
 x = []; y = []
-with open('with_shuffle_results/result10.csv', newline='') as csvfile:
+with open('without_shuffle_results/result1.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile)
     for i, row in enumerate(spamreader):
         if i >= 2:
 #            print(int(row[0]), float(row[1]))
-            x.append(str(row[0]))
-            y.append(float(row[1]))
+            x.append(math.log(int(row[0]), 2))
+            y.append(math.log(float(row[1]), 2))
     print ('\n')
     
-plt.figure(figsize=(20, 10))
-plt.plot(x, y, color = 'blue',marker = 'o', markerfacecolor = 'black', markersize = 7)
-plt.xticks(rotation=90)
+plt.figure(figsize=(10, 6))
+plt.plot(x, y, color = 'r',marker = 'o', markerfacecolor = 'blue', markersize = 6)
+#plt.xticks(rotation=90)
 plt.xlabel('Size')
 plt.ylabel('Runtime')
     
+plt.legend()
 plt.show()
